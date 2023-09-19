31255091148829ad9c3466e8929118cd12fd4dd0
SrDaviid <luis.david.08@hotmail.com>
2023-09-19 15:17:49 -0400

    less space

--- lib/services/infrastructure/datasources/moviedb_datasource.impl.dart
+++ lib/services/infrastructure/datasources/moviedb_datasource.impl.dart
@@ -22,6 +22,7 @@ class MoviedbDatasource extends MoviesDatasource {
   @override
   Future<List<Movie>> getNowPlaying({int page = 1}) async {
     final response = await dio.get('/movie/now_playing');
+
     final movieDBResponse = MovieDbResponse.fromJson(response.data);
 
     final List<Movie> movies = movieDBResponse.results
