d5002be0f355081abb4af01adf31318bb6809df4
SrDaviid <luis.david.08@hotmail.com>
2023-09-20 08:29:27 -0400

    add news properties to card swiper

--- lib/widgets/movies/movies_slideshow.dart
+++ lib/widgets/movies/movies_slideshow.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:card_swiper/card_swiper.dart';
 import 'package:flutter/material.dart';
 import 'package:movie_app_2/models/models.dart';
@@ -16,8 +17,12 @@ class MoviesSlidershow extends StatelessWidget {
         viewportFraction: 0.8,
         //This property sets a distance between these card swiper
         scale: 0.9,
-        //This property 
+        //This property
         autoplay: true,
+        pagination: const SwiperPagination(
+            margin: EdgeInsets.only(top: 0),
+            builder: DotSwiperPaginationBuilder(
+                activeColor: Colors.indigo, color: Colors.black38)),
         itemCount: movies.length,
         itemBuilder: (context, index) {
           final movie = movies[index];
@@ -30,10 +35,37 @@ class MoviesSlidershow extends StatelessWidget {
 
 class _Slide extends StatelessWidget {
   final Movie movie;
+
   const _Slide({required this.movie});
 
   @override
   Widget build(BuildContext context) {
-    return Placeholder();
+    final decoration = BoxDecoration(
+      borderRadius: BorderRadius.circular(20),
+      boxShadow: const [
+        BoxShadow(color: Colors.black45, blurRadius: 10, offset: Offset(0, 10))
+      ],
+    );
+
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 30),
+      child: DecoratedBox(
+        decoration: decoration,
+        child: ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: Image.network(
+              movie.backdropPath,
+              fit: BoxFit.cover,
+              loadingBuilder: (context, child, loadingProgress) {
+                if (loadingProgress != null) {
+                  return const DecoratedBox(
+                      decoration: BoxDecoration(color: Colors.black12));
+                }
+
+                return FadeIn(child: child);
+              },
+            )),
+      ),
+    );
   }
 }
