074a383ba0e8e710d28c2eacbe435df0fed949be
SrDaviid <luis.david.08@hotmail.com>
2023-09-25 18:24:15 -0400

    add getPopularMovies datasources (endpoints too)

--- lib/services/datasources/movies_datasources.dart
+++ lib/services/datasources/movies_datasources.dart
@@ -2,4 +2,6 @@ import '../../models/movie_models.dart';
 
 abstract class MoviesDatasource {
   Future<List<Movie>> getNowPlaying({int page = 1});
+  Future<List<Movie>> getPopularMovies({int page = 1});
+
 }
