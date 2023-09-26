a6560e2706b211fd07edbcb5bf3f3cf33978a804
SrDaviid <luis.david.08@hotmail.com>
2023-09-26 15:21:35 -0400

    add top rated movies datasource to get data

--- lib/services/datasources/movies_datasources.dart
+++ lib/services/datasources/movies_datasources.dart
@@ -4,5 +4,5 @@ abstract class MoviesDatasource {
   Future<List<Movie>> getNowPlaying({int page = 1});
   Future<List<Movie>> getPopularMovies({int page = 1});
   Future<List<Movie>> getUpComingMovies({int page = 1});
-
+  Future<List<Movie>> getTopRatedMovies({int page = 1});
 }
--- lib/services/infrastructure/datasources/moviedb_datasource.impl.dart
+++ lib/services/infrastructure/datasources/moviedb_datasource.impl.dart
@@ -53,4 +53,12 @@ class MoviedbDatasource extends MoviesDatasource {
 
     return _jsonToMovies(response.data);
   }
+
+  @override
+  Future<List<Movie>> getTopRatedMovies({int page = 1}) async {
+    final response =
+        await dio.get('/movie/top_rated', queryParameters: {'page': page});
+
+    return _jsonToMovies(response.data);
+  }
 }
