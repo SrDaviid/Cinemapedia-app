7cf69f6555cfed369cef3bdc3598b71909c9fe6b
SrDaviid <luis.david.08@hotmail.com>
2023-09-28 08:14:19 -0400

    Add new route "Movies" to navigate to the movie selected

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -8,6 +8,24 @@ final appRouter = GoRouter(
       path: '/',
       name: HomeScreen.name,
       builder: (context, state) => const HomeScreen(),
-    )
+      routes: [
+        GoRoute(
+          path: 'movie/:id',
+          name: MovieScreen.name,
+          builder: (context, state) {
+            final movieId = state.pathParameters['id'] ?? 'no-id';
+            return MovieScreen(movieId: movieId);
+          },
+        ),
+      ],
+    ),
+    // GoRoute(
+    //   path: '/movie/:id',
+    //   name: MovieScreen.name,
+    //   builder: (context, state) {
+    //     final movieId = state.pathParameters['id'] ?? 'no-id';
+    //     return MovieScreen(movieId: movieId);
+    //   },
+    //  ),
   ],
 );
--- lib/screens/screens.dart
+++ lib/screens/screens.dart
@@ -1,1 +1,2 @@
 export 'package:movie_app_2/screens/movies/home_screen.dart';
+export 'package:movie_app_2/screens/movies/movie_screen.dart';
