fac5c83da13731baf54e1c4a84e299bb7c365ea3
SrDaviid <luis.david.08@hotmail.com>
2023-12-06 12:55:52 -0400

    add getDayOfWeek and getMonth function

--- lib/config/helpers/human_formats.dart
+++ lib/config/helpers/human_formats.dart
@@ -1,11 +1,26 @@
 import 'package:intl/intl.dart';
 
 class HumanFormats {
-  static String number(double number) {
-    final formatterNumber =
-        NumberFormat.compactCurrency(decimalDigits: 0, symbol: '', locale: 'en')
-            .format(number);
+  static String number(double number, [int decimals = 0]) {
+    final formatterNumber = NumberFormat.compactCurrency(
+      decimalDigits: decimals,
+      symbol: '',
+      locale: 'en',
+    ).format(number);
 
     return formatterNumber;
   }
+}
+
+String getDayOfWeek() {
+  DateTime now = DateTime.now();
+  String dayOfWeek = DateFormat('EEEE').format(now);
+  String dayOfMonth = DateFormat('d').format(now);
+  return '$dayOfWeek  $dayOfMonth';
+}
+
+String getMonth() {
+  DateTime now = DateTime.now();
+  String actualMonth = DateFormat('MMMM').format(now);
+  return actualMonth;
 }
