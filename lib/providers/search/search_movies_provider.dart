45b2f41f17d738ecd098eea435450ff98227d760
SrDaviid <luis.david.08@hotmail.com>
2024-03-26 10:52:27 -0400

    Implement SearchMoviesProvider that allows me to keep the search

--- lib/providers/search/search_movies_provider.dart
+++ lib/providers/search/search_movies_provider.dart
@@ -1,11 +1,30 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
+import 'package:movie_app_2/providers/movies/movies_repository_provider.dart';
 import '../../models/models.dart';
 
 final searchQueryProvider = StateProvider<String>((ref) => '');
 
-// final searchedMoviesProvider = StateNotifierProvider<notifier, state>((ref) {});
+final searchedMoviesProvider =
+    StateNotifierProvider<SearchedMoviesNotifier, List<Movie>>((ref) {
+  final moviesRepository = ref.read(movieRepositoryProvider);
+
+  return SearchedMoviesNotifier(
+      searchMovies: moviesRepository.searchMovie, ref: ref);
+});
+
+typedef SearchMoviesCallback = Future<List<Movie>> Function(String query);
 
 class SearchedMoviesNotifier extends StateNotifier<List<Movie>> {
-  SearchedMoviesNotifier() : super([]);
+  final SearchMoviesCallback searchMovies;
+  final Ref ref;
+
+  SearchedMoviesNotifier({required this.searchMovies, required this.ref})
+      : super([]);
+
+  Future<List<Movie>> searchMoviesByQuery(String query) async {
+    final List<Movie> movies = await searchMovies(query);
+    ref.read(searchQueryProvider.notifier).update((state) => query);
+    state = movies;
+    return movies;
+  }
 }
--- lib/widgets/shared/custom_appbar.dart
+++ lib/widgets/shared/custom_appbar.dart
@@ -33,18 +33,17 @@ class CustomAppbar extends ConsumerWidget {
               const Spacer(),
               IconButton(
                 onPressed: () {
-                  final moviesRepository = ref.read(movieRepositoryProvider);
+                  //TODO:
+                  // final searchedMovies = ref.read(searchedMoviesProvider);
                   final searchQuery = ref.read(searchQueryProvider);
 
                   showSearch<Movie?>(
                     query: searchQuery,
                     context: context,
-                    delegate: SearchMovieDelegate(searchMovies: (query) {
-                      ref
-                          .read(searchQueryProvider.notifier)
-                          .update((state) => query);
-                      return moviesRepository.searchMovie(query);
-                    }),
+                    delegate: SearchMovieDelegate(
+                        searchMovies: ref
+                            .read(searchedMoviesProvider.notifier)
+                            .searchMoviesByQuery),
                   ).then(
                     (movie) {
                       if (movie == null) return;
